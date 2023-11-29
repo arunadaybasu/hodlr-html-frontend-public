201c6b919124224b5aea1e63093628b3806d6019
Arunaday Basu <arunadaybasu@gmail.com>
2023-11-29 11:14:30 +0530

    Updates

--- js/dashboard/dashboard-2.js
+++ js/dashboard/dashboard-2.js
@@ -5,58 +5,25 @@
 	
  var dzChartlist = function(){
 	
-	var screenWidth = $(window).width();	
+	var screenWidth = $(window).width(),
+	dataSeriesShibUsdt = [],
+	chartMarketPriceShib,
+	trigoStrength = 3,
+	iteration = 11,
+	shibMin = 9999999999999,
+	shibMax = 0;
 
-	// window.Apex = {
-	//   chart: {
-	//     foreColor: '#fff',
-	//     toolbar: {
-	//       show: false
-	//     },
-	//   },
-	//   colors: ['#FCCF31', '#17ead9', '#f02fc2'],
-	//   stroke: {
-	//     width: 3
-	//   },
-	//   dataLabels: {
-	//     enabled: false
-	//   },
-	//   grid: {
-	//     borderColor: "#40475D",
-	//   },
-	//   xaxis: {
-	//     axisTicks: {
-	//       color: '#333'
-	//     },
-	//     axisBorder: {
-	//       color: "#333"
-	//     }
-	//   },
-	//   fill: {
-	//     type: 'gradient',
-	//     gradient: {
-	//       gradientToColors: ['#F55555', '#6078ea', '#6094ea']
-	//     },
-	//   },
-	//   tooltip: {
-	//     theme: 'dark',
-	//     x: {
-	//       formatter: function (val) {
-	//         return moment(new Date(val)).format("HH:mm:ss")
-	//       }
-	//     }
-	//   },
-	//   yaxis: {
-	//     decimalsInFloat: 2,
-	//     opposite: true,
-	//     labels: {
-	//       offsetX: -10
-	//     }
-	//   }
-	// };
+	const URL = 'http://localhost:4000';
+  const socket = io(URL, {
+  	cors: {
+			origin: URL
+	  }
+  });
 
-	var trigoStrength = 3
-	var iteration = 11
+  const form = document.getElementById('chat-form-1');
+  const input = document.getElementById('chat-input-1');
+  const messages = document.getElementById('chat-messages-1');
+
 
 	function getRandom() {
 	  var i = iteration;
@@ -122,81 +89,149 @@
 		chart.render();
 	}
 	
-	var marketChart = function(){
-		 var options = {
-        series: [
-	        {
-	          name: 'series1',
-	          data: [200, 200, 200, 450, 300, 400, 300,400, 500, 300]
-	        },
-	        {
-	          name: 'series2',
-	          data: [400, 300, 450, 350, 700, 200, 800, 800, 700, 750]
-	        }
-        ],
-        chart: {
-          height: 350,
-          type: 'line',
-				  toolbar:{
-					  show:false
-				  }
-    		},
-				colors:["#2258BF","#FF7213"],
-        dataLabels: {
-          enabled: false
-        },
-        stroke: {
-          curve: 'smooth',
-		   		width: 10,
-        },
-				legend:{
-					show:false
-				},
-				grid:{
-					borderColor: '#AFAFAF',
-					strokeDashArray: 10,
-				},
-				yaxis: {
-				  labels: {
+	var marketChart = async function() {
+
+		var options = {
+			series: [
+			  {
+			    name: 'SHIB/USDT',
+			    data: dataSeriesShibUsdt
+			  }
+			],
+			chart: {
+		    height: 350,
+		    type: 'line',
+		    stacked: true,
+		    animations: {
+		      enabled: true,
+		      easing: 'linear',
+		      dynamicAnimation: {
+		        speed: 1000
+		      }
+		    },
+		    dropShadow: {
+		      enabled: true,
+		      opacity: 0.3,
+		      blur: 5,
+		      left: -7,
+		      top: 22
+		    },
+		    events: {
+		      // animationEnd: function (chartContext, chartOptions) {
+		      //   const newData1 = chartContext.w.config.series[0].data.slice();
+		      //   newData1.shift();
+		      //   // check animation end event for just 1 series to avoid multiple updates
+		      //   if (chartOptions.el.node.getAttribute('index') === '0') {
+		      //     window.setTimeout(function () {
+		      //       chartContext.updateOptions({
+		      //         series: [{
+		      //           data: newData1
+		      //         }]
+		      //       }, false, false)
+		      //     }, 300)
+		      //   }
+		      // }
+		    },
+		    toolbar: {
+		      show: false
+		    },
+		    zoom: {
+		      enabled: false
+		    }
+		  },
+			colors:["#2258BF"],
+			dataLabels: {
+			  enabled: false
+			},
+			stroke: {
+			  curve: 'smooth',
+				width: 10
+			},
+			legend:{
+				show:false
+			},
+			grid:{
+				borderColor: '#AFAFAF',
+				strokeDashArray: 10
+			},
+			yaxis: {
+				min: 0,
+			  max: 1,
+			  labels: {
 					style: {
 						colors: '#787878',
 						fontSize: '13px',
 						fontFamily: 'Poppins',
 						fontWeight: 400
-						
 					},
-					formatter: function (value) {
-					  return value + "k";
-					}
-				  },
+					// formatter: function (value) {
+					//   return value + "k";
+					// }
+			  }
+			},
+			xaxis: {
+			  type: 'datetime',
+			  labels:{
+				  style: {
+						colors: '#787878',
+						fontSize: '13px',
+						fontFamily: 'Poppins',
+						fontWeight: 400
+					},
+			  },
+			  axisBorder: {
+					show:false,  
+			  },
+			  axisTicks:{
+				  show: false,
 				},
-        xaxis: {
-          categories: ["Week 01","Week 02","Week 03","Week 04","Week 05","Week 06","Week 07","Week 08","Week 09","Week 10"],
-				  labels:{
-					  style: {
-							colors: '#787878',
-							fontSize: '13px',
-							fontFamily: 'Poppins',
-							fontWeight: 400
-						},
-				  },
-				  axisBorder: {
-						show:false,  
-				  },
-				  axisTicks:{
-					  show: false,
-					},
-			  
-	        },
-	        tooltip: {
-	          x: {
-	            format: 'dd/MM/yy HH:mm'
-	          },
-	        },
-        };
+		  },
+		  tooltip: {
+		    x: {
+		      format: 'dd/MM/yy HH:mm:ss'
+		    }
+		  },
+		  legend: {
+		    show: true,
+		    floating: true,
+		    horizontalAlign: 'left',
+		    onItemClick: {
+		      toggleDataSeries: false
+		    },
+		    position: 'top',
+		    offsetY: -28,
+		    offsetX: 60
+		  },
+		  title: {
+		    text: 'Price/USDT',
+		    align: 'left',
+		    style: {
+		      fontSize: '12px'
+		    }
+		  },
+		  subtitle: {
+		    text: 'SHIB/USDT',
+		    floating: true,
+		    align: 'right',
+		    offsetY: 0,
+		    style: {
+		      fontSize: '22px'
+		    }
+		  }
+		};
 
-        var chart = new ApexCharts(document.querySelector("#marketChart"), options);
-        chart.render();
+		chartMarketPriceShib = new ApexCharts(document.querySelector("#marketChart"), options);
+		chartMarketPriceShib.render();
+
+		// socket.on('price-shib-usdt', (msg) => {
+
+		// 	console.log('Update Price');
+
+		// 	dataSeriesShibUsdt.push( [msg.timestamp, msg.result.price] );
+		// 	chartMarketPriceShib.render();
+
+		// });
+
 	}
 
 	var realtimeLineChart = function(){
@@ -237,7 +272,7 @@
 		                data: newData2
 		              }],
 		              subtitle: {
-		                text: parseInt(getRandom() * Math.random()).toString(),
+		                text: ''+parseInt(getRandom() * Math.random()).toString(),
 		              }
 		            }, false, false)
 		          }, 300)
@@ -394,6 +429,65 @@
 			}
 		})
 	}
+
+	socket.on('price-shib-usdt-init', (msg) => {
+
+    console.log(msg);
+
+    for( var i = 0; i < msg.length; i++)
+    {
+    	dataSeriesShibUsdt.push( [msg[i].timestamp, msg[i].result.price] );
+    	if (parseFloat(msg[i].result.price) < shibMin)
+    		shibMin = parseFloat(msg[i].result.price);
+    	if (parseFloat(msg[i].result.price) > shibMax)
+    		shibMax = parseFloat(msg[i].result.price);
+    }
+    console.log(shibMin, shibMax);
+    
+		chartMarketPriceShib.updateOptions({
+		  series: [
+			  {
+			    name: 'SHIB/USDT',
+			    data: dataSeriesShibUsdt
+			  }
+			],
+		  yaxis: {
+				min: shibMin,
+			  max: shibMax,
+		  }
+		});
+
+  });
+
+	socket.on('price-shib-usdt', (msg) => {
+
+    console.log(msg);
+
+    // const item = document.createElement('li');
+    // item.textContent = msg.result.price;
+    // messages.appendChild(item);
+
+    // dataSeriesShibUsdt.push( [msg.timestamp, parseFloat(msg.result.price)] );
+
+    if (parseFloat(msg.result.price) < shibMin)
+  		shibMin = parseFloat(msg.result.price);
+  	if (parseFloat(msg.result.price) > shibMax)
+  		shibMax = parseFloat(msg.result.price);
+
+		chartMarketPriceShib.appendSeries({
+			name: 'SHIB/USDT',
+			data: [msg.timestamp, parseFloat(msg.result.price)]
+		});
+
+  });
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (input.value) {
+      socket.emit('chat-message-2', input.value);
+      input.value = '';
+    }
+  });
 	
 	/* Function ============ */
 		return {
